3d7715924f4c06a4f123c20cff83c6bad34b9a74
viciousvs <standalonebutterfly@gmail.com>
2022-09-13 20:35:09 +0600

    added ch 5

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 // Обработчик главной странице.
@@ -21,12 +23,42 @@ func home(w http.ResponseWriter, r *http.Request) {
 
 // Обработчик для отображения содержимого заметки.
 func showSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Отображение заметки..."))
+	// Извлекаем значение параметра id из URL и попытаемся
+	// конвертировать строку в integer используя функцию strconv.Atoi(). Если его нельзя
+	// конвертировать в integer, или значение меньше 1, возвращаем ответ
+	// 404 - страница не найдена!
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+
+	// Используем функцию fmt.Fprintf() для вставки значения из id в строку ответа
+	// и записываем его в http.ResponseWriter.
+	fmt.Fprintf(w, "Отображение выбранной заметки с ID %d...", id)
 }
 
 // Обработчик для создания новой заметки.
 func createSnippet(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Форма для создания новой заметки..."))
+	// Используем r.Method для проверки, использует ли запрос метод POST или нет. Обратите внимание,
+	// что http.MethodPost является строкой и содержит текст "POST".
+	if r.Method != http.MethodPost {
+		// Используем метод Header().Set() для добавления заголовка 'Allow: POST' в
+		// карту HTTP-заголовков. Первый параметр - название заголовка, а
+		// второй параметр - значение заголовка.
+		w.Header().Set("Allow", http.MethodPost)
+		w.Header().Set("Content-Type", "text/json; charset=utf-8")
+
+		// Вызываем метод w.WriteHeader() для возвращения статус-кода 405
+		// и вызывается метод w.Write() для возвращения тела-ответа с текстом "Метод запрещен".
+		http.Error(w, fmt.Sprintf("Метод %s запрещен", r.Method), http.StatusMethodNotAllowed)
+
+		// Затем мы завершаем работу функции вызвав "return", чтобы
+		// последующий код не выполнялся.
+		return
+	}
+
+	w.Write([]byte("Создание новой заметки..."))
 }
 
 func main() {
